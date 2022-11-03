f482198ed4ac1a6a3b8f9a19a7d7950241422d4e
SebastienJ27 <55695893+SebastienJ27@users.noreply.github.com>
2022-11-03 00:45:02 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from './html/Login';
+//import Registration from './html/Registration';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!</h1>
-    </div>
-  );
+    <Login />
+    /*<BrowserRouter>
+    <Routes>
+      <Route path="/" index element={<Login />} />
+      <Route path="/cards" index element={<Registration />} />
+    </Routes>
+  </BrowserRouter>*/
+
+);
 }
 
 export default App;
