9a79cf09494ac7b726a6b1fc4e2d67c0230dc608
jkglaspey <jkglaspey@gmail.com>
2022-11-03 13:27:05 -0400

    Merged email verification to main

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const {register,login, logout, loginRequired, profile,} = require("../controllers/auth");
+const {register,login, logout, loginRequired, profile, emailVerify} = require("../controllers/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout",loginRequired,logout);
 router.get("/profile",loginRequired, profile);
+router.get("/emailVerify/:token", emailVerify);
 
 module.exports = router;
