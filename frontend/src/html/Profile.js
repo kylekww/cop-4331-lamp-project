38a8ba7b745a1cc0037459d1cf13e6b1b7737d84
joshfeds <joshfeds14@gmail.com>
2022-11-08 00:39:21 -0500

    testing profile pt2

--- frontend/src/html/Profile.js
+++ frontend/src/html/Profile.js
@@ -1,12 +1,9 @@
 import { ListItemSecondaryAction } from '@mui/material';
-import React, { useState, setState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Profile.css';
 
 function Profile() {
-    const user = {
-        username: "",
-        name: ""
-    }
+    const [users, setUsers] = useState();
     const viewProfile = async event => 
     {
         const data = await fetch("/api/v1/auth/profile", {
@@ -16,13 +13,11 @@ function Profile() {
             },
         })
         .then(res => {
-          console.log(res);
           res.json().then((data) => {
             console.log(data);
             console.log(data.user.name);
             console.log(data.user.username);
-            user.name = data.user.name;
-            user.username = data.user.username;
+            
           }) 
         })
         .catch(err => {
@@ -30,7 +25,6 @@ function Profile() {
         });
        //this.setState
     };
-    
     const logOut = async event => 
     {
         const data = await fetch("/api/v1/auth/logout", {
@@ -46,20 +40,22 @@ function Profile() {
           console.log(err);
         });
     };
+    useEffect(() => {
+        viewProfile();
+      }, []);
     return (
         
         <div class ="container">
             <h1>Logo goes here</h1>
             <div class = "squarebg">
                 <h1>Profile View</h1>
-                <p id = "profile"></p>{
-                    user.map((item) => (
-                        <ol key = { item.user } >
-                        User_Name: { item.username }, 
-                        Full_Name: { item.name }
-                        </ol>   
-                    ))
-                }
+
+                {users && users.map((user) => {
+                    <p id = "profile">
+                        Username: {user.username}
+                        Full Name:{user.name}
+                    </p>
+                })}
                     
                     <button type = "button" class = "logout" onClick = {logOut}>Logout</button>
                     
