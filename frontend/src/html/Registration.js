bcea51562cba6a2760a9aaaf6c9f07356d5f3f23
SebastienJ27 <55695893+SebastienJ27@users.noreply.github.com>
2022-11-20 15:41:03 -0500

    Add files via upload

--- frontend/src/html/Registration.js
+++ frontend/src/html/Registration.js
@@ -9,47 +9,105 @@ function Registration() {
 
         setColor(randomColor);
     };*/
-    
-    const doRegistration = async event => 
-    {
+
+    function passwordRequirements(password) {
+        let upperCase = false;
+        let lowerCase = false;
+        let passwordNumber = false;
+        let passwordLength = false;
+
+        if (password.length >= 8) {
+            passwordLength = true;
+        }
+
+        for (let i = 0; i < password.length; i++) {
+            if (password[i] >= "0" && password[i] <= "9") {
+                passwordNumber = true;
+            } else if (password[i].toUpperCase() === password[i]) {
+                upperCase = true;
+            } else if (password[i].toUpperCase() !== password[i]) {
+                lowerCase = true;
+            }
+        }
+
+        if (passwordLength && passwordNumber && upperCase && lowerCase) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validEmail(email) {
+        let knightsEmail = "@knights.ucf.edu";
+        const knightsEmailArray = knightsEmail.split("")
+        const emailArray = email.split("")
+        let validation = false;
+        let j = 0;
+
+        for (let i = (emailArray.length - knightsEmailArray.length); i < emailArray.length; i++) {
+            if (knightsEmailArray[j] == emailArray[i]) {
+                console.log("good on: " + emailArray[i]);
+                validation = true;
+                j++;
+            } else {
+                validation = false;
+                console.log("bad on : " + emailArray[i]);
+            }
+        }
+        return validation;
+    }
+
+    const doRegistration = async event => {
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
         const name = document.getElementById("name").value
         const email = document.getElementById("email").value;
         const color = "#" + Math.floor(Math.random() * 16777215).toString(16);  // This needs to be added to each user on registration
-        const data = await fetch("/api/v1/auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                username,
-                password,
-                name,
-                email,
-                color
-                //color
-            }),
-        }).then(res => {
-          if(res.status !== 201) alert("Invalid registration");
-          else {
-            alert("Registration was successful.");
-            window.location.href = '/';
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-        
-        // This is a bootleg solution. "Might" need attention.
-        console.log(data);
+
+        if ((passwordRequirements(password)) && (validEmail(email))) {
+            const data = await fetch("/api/v1/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    name,
+                    email,
+                    color
+                }),
+            }).then(res => {
+                if (res.status !== 201) alert("Invalid registration");
+                else {
+                    alert("Registration was successful.");
+                    window.location.href = '/';
+                }
+            })
+                .catch(err => {
+                    console.log(err);
+                });
+
+            // This is a bootleg solution. "Might" need attention.
+            console.log(data);
+        } else if ((!passwordRequirements(password)) && validEmail(email)) {
+            console.log("Password must meet all requirements!");
+        }
+        else if (passwordRequirements(password) && (!validEmail(email))) {
+            console.log("You must use your knights email!");
+        }
+        else {
+            console.log("Password must meet all requirements!");
+            console.log("You must use your knights email!");
+        }
+
     };
     return (
-        <div class ="container">
+        <div class="container">
             <h1>Logo goes here</h1>
-            <div class = "squarebg">
+            <div class="squarebg">
                 <h1>Signup </h1>
-                <input type="text" id="name" placeholder="First  Name" required />
+                <input type="text" id="name" placeholder="Name" required />
                 <input type="text" id="username" placeholder="Username" required />
                 <input type="text" id="email" placeholder="Email" required />
                 <input type="password" id="password" placeholder="Password" required />
@@ -61,10 +119,10 @@ function Registration() {
                     1 Lower Case Character <br />
                 </span>
                 <p id="noerror">Error: Username already taken!</p>
-                <button type = "button" class = "button" onClick = {doRegistration}>Create Account</button>
+                <button type="button" class="button" onClick={doRegistration}>Create Account</button>
                 <p>Already have an account? <a href="/">Login!</a></p>
-            </div>   
-        </div>  
+            </div>
+        </div>
     );
 };
 
