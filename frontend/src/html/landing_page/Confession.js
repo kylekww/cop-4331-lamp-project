cc04a4e728c75cc82eb75f20818f9c1c23d29232
jkglaspey <jkglaspey@gmail.com>
2022-11-22 14:13:44 -0500

    Added options menu to confessions

--- frontend/src/html/landing_page/Confession.js
+++ frontend/src/html/landing_page/Confession.js
@@ -1,16 +1,37 @@
 import '../../css/styles.css';
-import React, { useState, useEffect } from 'react';
-import Badge from '@mui/material/Badge';
+import React, { useState, useEffect, MouseEvent } from 'react';
+import { MenuItem, Menu, ListItemIcon, ListItemText, Badge, Tooltip, IconButton } from '@mui/material';
 import CommentIcon from '@mui/icons-material/Comment';
-import IconButton from '@mui/material/IconButton';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import EditIcon from '@mui/icons-material/Edit';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Confession(Props) {
-
+  // Post info
   const[post, setPost] = useState([]);
+  
+  // Edit menu logic
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleOptionsClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleOptionsClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEditPost = () => {
+    setAnchorEl(null);
+    console.log("Edit post");
+  };
+  const handleDeletePost = () => {
+    setAnchorEl(null);
+    console.log("Delete post");
+  };
 
+
+  // React hook for confessions
   useEffect(() => {
     const displayPosts = async event =>
     {
@@ -58,55 +79,92 @@ function Confession(Props) {
           {post.map(post => ( 
             <div className = "confessionPost">
               <div className = "confessionPostWrapper">
+
                 <div className = "confessionPostEdit">
                   <div className = "confessionPostEditButton">
-                    <IconButton disabled style={{
-                          color: "#BABABA",
-                    }}>
-                      <EditIcon sx={{ fontSize: 30 }}/>
-                    </IconButton>
+                    <Tooltip title="Options">
+                      <IconButton 
+                        id="edit-button" 
+                        onClick={ handleOptionsClick } 
+                        aria-controls={open ? 'edit-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        style={{
+                            color: "#BABABA",
+                      }}>
+                        <MoreHorizIcon sx={{ fontSize: 40 }}/>
+                      </IconButton>
+                    </Tooltip>
                   </div>
+                  <Menu 
+                    elevation={0}
+                    id="edit-menu"
+                    anchorEl={ anchorEl }
+                    open={ open }
+                    onClose={ handleOptionsClose }
+                    MenuListProps={{ 'aria-labelledby': 'edit-button' }}
+                  >
+                    <MenuItem elevation={0} onClick={ handleEditPost }>
+                      <ListItemIcon>
+                        <EditIcon sx={{ fontSize: 25 }}/>
+                      </ListItemIcon>
+                      <ListItemText>Edit Confession</ListItemText>
+                    </MenuItem>
+                    <MenuItem elevation={0} onClick={ handleDeletePost }>
+                      <ListItemIcon>
+                        <DeleteIcon sx={{ fontSize: 25 }}/>
+                      </ListItemIcon>
+                      <ListItemText>Delete Confession</ListItemText>
+                    </MenuItem>
+                  </Menu>
                 </div>
+
                 <div className='confessionText'>
                     {post.confession}
-                  </div>
-                  <div className= 'confessionVotesComments'>
-                  
-                    <div className = 'votes'>
+                </div>
+
+                <div className= 'confessionVotesComments'>
+                  <div className = 'votes'>
+                    <Tooltip title="Upvote">
                       <IconButton onclick={ upvoteConfession } style={{
                           color: "#BABABA",
                         }}>
                           <KeyboardArrowUpIcon sx={{ fontSize: 50 }}/>
                       </IconButton>
-                      <Badge badgeContent={1000} max={999} sx={{
-                        "& .MuiBadge-badge": {
-                          backgroundColor: Props.isHot ? "#463bdd" : "rgba(207, 15, 15, 0.9)",
-                          color: "white",
-                          fontSize: 20,
-                          height: 30
-                      }}}>
-                      </Badge>
+                    </Tooltip>
+                    <Badge badgeContent={1000} max={999} sx={{
+                      "& .MuiBadge-badge": {
+                        backgroundColor: Props.isHot ? "#463bdd" : "rgba(207, 15, 15, 0.9)",
+                        color: "white",
+                        fontSize: 20,
+                        height: 30
+                    }}}>
+                    </Badge>
+                    <Tooltip title="Downvote">
                       <IconButton onclick={ downvoteConfession } style={{
                           color: "#BABABA"
                         }}>
                           <KeyboardArrowDownIcon sx={{ fontSize: 50 }}/>
                       </IconButton>
-                    </div>
+                    </Tooltip>
+                  </div>
 
-                    <div className='comments'>
-                      <Badge badgeContent={100} max={99} sx={{
-                        "& .MuiBadge-badge": {
-                          backgroundColor: Props.isHot ? "#463bdd" : "rgba(207, 15, 15, 0.9)",
-                          color: "white"
-                      }}}>
+                  <div className='comments'>
+                    <Badge badgeContent={100} max={99} sx={{
+                      "& .MuiBadge-badge": {
+                        backgroundColor: Props.isHot ? "#463bdd" : "rgba(207, 15, 15, 0.9)",
+                        color: "white"
+                    }}}>
+                      <Tooltip title="Add Comment">
                         <IconButton onClick={ clickCommentButton } style={{
                           color: "#BABABA"
                         }}>
                           <CommentIcon sx={{ fontSize: 30 }}/>
                         </IconButton>
-                      </Badge>
-                    </div>
+                      </Tooltip>
+                    </Badge>
                   </div>
+                </div>
               </div>
             </div>
             ))}
