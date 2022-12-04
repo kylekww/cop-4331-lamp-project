deca0854ccb2c6c9b00928ae510576bdf24e90ff
Jyke321 <jyke321@gmail.com>
2022-12-04 17:33:08 -0500

    dialog modal added

--- mobile/screens/JS/LandingPage/NewConfessionButton.js
+++ mobile/screens/JS/LandingPage/NewConfessionButton.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Modal } from 'react-native';
 //import DialogNewConfession from './DialogNewConfession';
 
 /* Need to be added:
@@ -12,48 +12,67 @@ export default function NewConfessionButton(Props) {
   const newButtonColor = ['#DE621C','rgba(128,199,239,.5)'];
 
   const handleClickOpen = () => {
-    //setOpen(true);
+    setOpen(true);
   };
 
   const handleClose = () => {
-    //setOpen(false);
+    setOpen(false);
   };
 
   return (
     //<DialogNewConfession open={open} handleClose={handleClose}></DialogNewConfession>
-    <View>
+    <View style={styles.container}>
       <Modal
         animationType="slide"
         transparent={true}
-        visible={modalVisible}
+        visible={open}
+        presentationStyle="pageSheet"
         onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
+          setOpen(!open);
         }}
       >
-        <TouchableOpacity onPress={handleClickOpen} style={[styles.button, {backgroundColor:newButtonColor[(Props.isNew?1:0)]}]}>
-            <Text styles={styles.text}>Got something to confess?</Text>
-        </TouchableOpacity>
+        <View style={[styles.modalView, {backgroundColor:newButtonColor[(Props.isNew?1:0)]}]}>
+          <TextInput styles={styles.text}
+            placeholder="What's on your mind?">
+          </TextInput>
+        </View>
       </Modal>
+      <TouchableOpacity onPress={handleClickOpen} style={[styles.button, {backgroundColor:newButtonColor[(Props.isNew?1:0)]}]}>
+          <Text styles={styles.text}>What's on your mind?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({ 
-    text: {
-        alignSelf: 'center',
-        fontSize: 30,
-        color: 'white',
-    },
-    button: {
-        padding: 10,
-        bottom: 20,
-        alignItems: 'center',
-        alignSelf: 'center',
-        flexWrap: 'wrap',
-        borderWidth: 5,
-        borderRadius: 30,
-        width: '70%',
-        height: 60,
-    },
+  container: {
+    position: 'relative',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+  },
+  modalView: {
+    margin: 20,
+    top:'20%',
+    flexWrap: 'wrap',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: "center",
+    height:'80%',
+  },
+  text: {
+      alignSelf: 'center',
+      fontSize: 30,
+      color: 'white',
+  },
+  button: {
+      padding: 10,
+      alignItems: 'center',
+      alignSelf: 'center',
+      flexWrap: 'wrap',
+      borderWidth: 5,
+      borderRadius: 30,
+      width: '70%',
+      height: 60,
+  },
 });
