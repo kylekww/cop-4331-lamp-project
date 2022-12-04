a2a039d3e53cb316ee356849b7fda29513a258b7
joshfeds <joshfeds14@gmail.com>
2022-12-04 00:49:07 -0500

    updated reg css from 2007 to 2009

--- frontend/src/html/Registration.js
+++ frontend/src/html/Registration.js
@@ -1,13 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import { yellow } from '@mui/material/colors';
+import React, { useState } from 'react';
 import '../css/styles.css';
+import LoginIcon from '@mui/icons-material/Login';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Registration() {
-    /* Leaving this here for now
-    const[color, setColor] = useState("#ffffff");
-    const randomizedColor = () => {
-        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-        setColor(randomColor);
-    };*/
+const theme = createTheme();
+export default function Registration() {
 
     function passwordRequirements(password) {
         let upperCase = false;
@@ -100,28 +111,120 @@ function Registration() {
         }
 
     };
-    return (
-        <div class="container">
-            <img src={require('../images/NewIcon.jpg')} class="HushIcon"/>
-            <div class="squarebg">
-                <h1>Signup </h1>
-                <input type="text" id="name" placeholder="Name" required />
-                <input type="text" id="username" placeholder="Username" required />
-                <input type="text" id="email" placeholder="Email" required />
-                <input type="password" id="password" placeholder="Password" required />
-                <span class="passwordRequirements">
-                    Requirements: <br />
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        
+        
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          > 
+          <Typography component="h1" variant="h5" color = 'rgb(227, 19, 19)'>
+              Hush UCF
+            </Typography>
+            <Avatar sx={{ m: 1, bgcolor: 'rgb(227, 19, 19)' }}>
+            <AppRegistrationIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" color = 'rgb(227, 19, 19)'>
+              Got a secret to share? Register Now!
+            </Typography>
+            <Box sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Full Name"
+                name="Full Name"
+                autoComplete="fullname"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Knights Email Account"
+                name="Email"
+                autoComplete="Email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick = {doRegistration}
+                sx={{ mt: 3, 
+                    mb: 2, 
+                    backgroundColor: 'rgb(227, 19, 19)',
+                    '&:hover': {
+                    backgroundColor: 'rgb(222, 98, 28)',
+                    color: '#ffffff',
+                }, }}
+                
+              >
+                Register
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                <span variant="body2">
+                    Password Requirements: <br />
                     8 Characters <br />
                     1 Number <br />
                     1 Upper Case Character <br />
                     1 Lower Case Character <br />
                 </span>
-                <p id="noerror">Error: Username already taken!</p>
-                <button type="button" class="button" onClick={doRegistration}>Create Account</button>
-                <p>Already have an account? <a href="/">Login!</a></p>
-            </div>
-        </div>
-    );
-};
+                </Grid>
+                <Grid item>
+                  <Link href="/" variant="body2">
+                    {"Already have an account? Login!"}
+                  </Link>
+                </Grid>
+              </Grid>
+              
+            </Box>
+          </Box>
+        </Grid>
 
-export default Registration;
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx = {{
+            backgroundImage: 'linear-gradient(rgb(222, 98, 28), rgb(227, 19, 19));'
+          }}
+          ><img src={require('../images/HotIcon.jpg')} class="HushIconLogin"/></Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
