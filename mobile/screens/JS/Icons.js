8411391f8dd800975623b502b752dd18b151be8f
Jyke321 <jyke321@gmail.com>
2022-12-04 23:01:51 -0500

    Icons

---
+++ mobile/screens/JS/Icons.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet, Image, Button, View, Text } from 'react-native';
+
+export default function Logo(Props) {
+    const style = Props.style;
+    const height = Props.height;
+    const width = Props.width;
+
+    //map of possible icon types
+    const iconMap = new Map();
+    iconMap.set('upvote',0);
+    iconMap.set('downvote',1);
+    //icon image locations
+    const upvote = 'https://github.com/kylekww/cop-4331-mern-project/blob/main/mobile/assets/NewIcon.jpg?raw=true';
+    const downvote = 'https://github.com/kylekww/cop-4331-mern-project/blob/main/mobile/assets/HotIcon.jpg?raw=true';
+    //accesible array of image locations
+    const iconType = [upvote,downvote]
+
+    return (
+        <View style = {[{height: height},{width: width}]}>
+            <Image
+                style={styles.logo}
+                source={{uri:(iconType[iconMap.get(style)]),}}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    logo: {
+        maxWidth: 432,
+        maxHeight: 432,
+        width: '100%',
+        height: '100%',
+    },
+});
--- mobile/screens/JS/LandingPage/Confession.js
+++ mobile/screens/JS/LandingPage/Confession.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { View , FlatList, StyleSheet, Text } from 'react-native';
+import Icons from '../Icons';
 
 import searchConfessions from './searchConfessions';
 
@@ -15,82 +16,96 @@ export default function Confession(Props) {
     post.length = 0;
     setIsNew(current => !current);
     Props.isNew ? setSearch(1) : setSearch(2)
-  }, [Props.isNew])
+  }, [Props.isNew]);
 
    // Edit menu logic
-   const [anchorEl, setAnchorEl] = React.useState(null);
-   const open = Boolean(anchorEl);
-   
-   const handleScroll = (e) => {
-     const bottom = Math.round(e.target.scrollHeight - e.target.scrollTop) === e.target.clientHeight;
-     if(bottom && !wasLastList){
-       if(post.length - 1 < 0){
-         //console.log('we must set oid to neutral')
-         setOid('');
-       }
-       //console.log(post[post.length - 1]._id)
-       setOid(post[post.length - 1]._id);
-     }
-   }
-
-    //const[vote, setVote] = useState(post.voteID.netVotes);
-    //const[interacted, setInteracted] = useState(post.userInteracted)
-    
-    //console.log(isNew._currentValue)
-    const handleOptionsClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleOptionsClose = () => {
-      setAnchorEl(null);
-    };
-    const handleEditPost = () => {
-      setAnchorEl(null);
-      //console.log("Edit post");
-    };
-    const handleDeletePost = () => {
-      setAnchorEl(null);
-      //console.log("Delete post");
-    };   
-      
-    const upvoteHelper = (e) => {
-      upvoteConfession(e.currentTarget.value);
-      if(interacted == 1){
-        //console.log('this user was interacted before the upvote')
-        
-        setInteracted(0)
-        setVote(vote - 1)
-      } 
-      if(interacted == 0){
-        //console.log('this user was not interacted before the upvote')
-        setInteracted(1)
-        setVote(vote + 1)
-      }
-      if(interacted == -1){
-        //console.log('downvoted before vote, now upvoted')
-        setInteracted(1)
-        setVote(vote + 2)
-      }
-    }
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
   
-    const downvoteHelper = (e) => {
-      downvoteConfession(e.currentTarget.value);
-      if(interacted == -1){
-          //console.log('downvoted before, now neutral')
-          setInteracted(0)
-          setVote(vote + 1)
+  const handleScroll = (e) => {
+    const bottom = Math.round(e.target.scrollHeight - e.target.scrollTop) === e.target.clientHeight;
+    if(bottom && !wasLastList){
+      if(post.length - 1 < 0){
+        //console.log('we must set oid to neutral')
+        setOid('');
       }
-      if(interacted == 0){
-          //console.log('neutral to downvoted')
-          setInteracted(-1)
-          setVote(vote - 1)
-      }
-      if(interacted == 1){
-          //console.log('upvote to downvote')
-          setInteracted(-1)
-          setVote(vote - 2)
-      }
-
+      //console.log(post[post.length - 1]._id)
+      setOid(post[post.length - 1]._id);
     }
+  }
+
+  //const[vote, setVote] = useState(post.voteID.netVotes);
+  //const[interacted, setInteracted] = useState(post.userInteracted)
+  
+  //console.log(isNew._currentValue)
+  const handleOptionsClick = (event) => {
+    event.currentTarget.disabled = true;
+    setAnchorEl(event.currentTarget);
+  };
+  const handleOptionsClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEditPost = (e) => {
+    //if(deleted) return null
+    e.currentTarget.disabled = true;
+    setAnchorEl(null);
+    
+    console.log("Edit post");
+  };
+  const handleDeletePost = (e) => {
+    //if(deleted) return null
+    e.currentTarget.disabled = true;
+    setAnchorEl(null);
+    deleteConfession(Props.post._id)
+    setDeleted(true)
+    console.log("Delete post");
+  }; 
+      
+  const upvoteHelper = (e) => {
+    e.currentTarget.disabled = true;
+    //if(deleted) return null
+    upvoteConfession(e.currentTarget.value);
+    if(interacted == 1){
+      console.log('this user was interacted before the upvote')
+      setInteracted(0)
+      setVote(vote - 1)
+    } 
+    if(interacted == 0){
+      console.log('this user was not interacted before the upvote')
+      setInteracted(1)
+      setVote(vote + 1)
+    }
+    if(interacted == -1){
+      console.log('downvoted before vote, now upvoted')
+      setInteracted(1)
+      setVote(vote + 2)
+    }
+  }
+  
+  const downvoteHelper = (e) => {
+    e.currentTarget.disabled = true;
+    downvoteConfession(e.currentTarget.value);
+    //if(deleted) return null
+    if(interacted == -1){
+      console.log('downvoted before, now neutral')
+      setInteracted(0)
+      setVote(vote + 1)
+    }
+    if(interacted == 0){
+      console.log('neutral to downvoted')
+      setInteracted(-1)
+      setVote(vote - 1)
+    }
+    if(interacted == 1){
+      console.log('upvote to downvote')
+      setInteracted(-1)
+      setVote(vote - 2)
+    }
+  }
+
+  function clickCommentButton() {
+    //window.location.href = '/comments/' + Props.post._id;
+  }
   
   const [tempData, setData] = useState([
     {name: 'Austin', username: 'test1', id: 1},
@@ -100,20 +115,6 @@ export default function Confession(Props) {
     {name: 'Austin', username: 'test1', id: 5},
     {name: 'Austin', username: 'test1', id: 6},
     {name: 'Austin', username: 'test1', id: 7},
-    {name: 'Austin', username: 'test1', id: 1+7},
-    {name: 'Austin', username: 'test1', id: 2+7},
-    {name: 'Austin', username: 'test1', id: 3+7},
-    {name: 'Austin', username: 'test1', id: 4+7},
-    {name: 'Austin', username: 'test1', id: 5+7},
-    {name: 'Austin', username: 'test1', id: 6+7},
-    {name: 'Austin', username: 'test1', id: 7+7},
-    {name: 'Austin', username: 'test1', id: 1+7+7},
-    {name: 'Austin', username: 'test1', id: 2+7+7},
-    {name: 'Austin', username: 'test1', id: 3+7+7},
-    {name: 'Austin', username: 'test1', id: 4+7+7},
-    {name: 'Austin', username: 'test1', id: 5+7+7},
-    {name: 'Austin', username: 'test1', id: 6+7+7},
-    {name: 'Austin', username: 'test1', id: 7+7+7},
 
   ]);
 
@@ -130,11 +131,30 @@ export default function Confession(Props) {
                 data={tempData}
                 renderItem={({ item }) => (
                   <View style = {styles.box} >
-                    <View>
-                      <TouchableOpacity onPress={pressHandler}>
-                        <Text style = {styles.text}>hi, this is item  {item.id}, the name is {item.name} 
-                        </Text>
-                      </TouchableOpacity>
+                    <View style={styles.columnSpace}>
+                      <Text style = {styles.text}>hi, this is item  {item.id}, the name is {item.name} 
+                      </Text>
+                      <View style = {styles.rowSpace}>
+                        <TouchableOpacity onPress={pressHandler}>
+                          <Text style = {styles.text}>delete</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={pressHandler}>
+                          <Text style = {styles.text}>edit</Text>
+                        </TouchableOpacity>
+                        <View style = {styles.rowSpace}>
+                          <TouchableOpacity onPress={pressHandler}>
+                          <Icons style = {'downvote'}
+                            height = {30} width = {30}
+                            />
+                          </TouchableOpacity>
+                          <Text style = {styles.text}>0</Text>
+                          <TouchableOpacity onPress={pressHandler}>
+                            <Icons style = {'upvote'}
+                            height = {30} width = {30}
+                            />
+                          </TouchableOpacity>
+                        </View>
+                      </View>
                     </View>
                   </View>
                 )}
@@ -257,5 +277,13 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     fontSize: 20,
     color: 'rgba(89,35,206,1)',
+  },
+  rowSpace: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  columnSpace: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
 });
