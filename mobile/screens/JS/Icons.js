3580c3e1856c056e1d1a9bf5f4ebb3ea5d5531b7
Jyke321 <jyke321@gmail.com>
2022-12-07 01:44:35 -0500

    Icons.js

--- mobile/screens/JS/Icons.js
+++ mobile/screens/JS/Icons.js
@@ -1,34 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Image, Button, View, Text } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function Logo(Props) {
     const style = Props.style;
     const height = Props.height;
     const width = Props.width;
-
-    //map of possible icon types
-    const iconMap = new Map();
-    iconMap.set('upvote',0);
-    iconMap.set('downvote',1);
-    iconMap.set('delete',2);
-    iconMap.set('comment',3);
-    //icon image locations
-    const upvote = 'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/up-arrow.webp';
-    const downvote = 'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/down-arrow.webp';
-    const deleteIcon = 'https://github.com/kylekww/cop-4331-mern-project/blob/main/mobile/assets/delete.png?raw=true';
-    const comment = 'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/comment.webp';
-    //accesible array of image locations
-    const iconType = [upvote,downvote,deleteIcon,comment]
+    const color = Props.color;
 
     return (
-        <View style = {[{height: height},{width: width}]}>
+        <View style = {[{height: height, width: width}]}>
             <Image
-                style={styles.logo}
-                source={{uri:(iconType[iconMap.get(style)]),}}
+                style={[styles.logo,{tintColor: color}]}
+                source={{uri:images.icons[style],}}
             />
         </View>
     )
 }
+const images = {
+    icons: {
+        'upvote' : 'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/up-arrow.webp',
+        'downvote' :'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/down-arrow.webp',
+        'delete' : 'https://github.com/kylekww/cop-4331-mern-project/blob/main/mobile/assets/delete.png?raw=true',
+        'comment' : 'https://raw.githubusercontent.com/kylekww/cop-4331-mern-project/main/mobile/assets/comment.webp',
+    }
+};
 
 const styles = StyleSheet.create({
     logo: {
